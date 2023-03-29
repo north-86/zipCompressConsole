8362545a762fdfd8d7686370e894ef7f600b81d0
northernman86 <aliaksandrprystauka@gmail.com>
2023-03-30 00:06:49 +0300

    Add changes.

--- Program.cs
+++ Program.cs
@@ -1,30 +1,55 @@
 ﻿using System.IO.Compression;
 using static System.Console;
 
-WriteLine("Select action:\n" + "1 - create archive\n" + "2 - unpack archive\n");
-string choice = ReadLine();
-WriteLine();
-switch (choice)
+bool isOpen = true;
+
+while (isOpen)
 {
-    case "1":
-        Write("Name of the source directory: ");
-        string sourceDirectoryName = ReadLine();
-        Write("File name of the target archive: ");
-        string destinationArchiveFileName = ReadLine();
-        ZipFile.CreateFromDirectory(sourceDirectoryName, destinationArchiveFileName, CompressionLevel.SmallestSize, true);
-        WriteLine();
-        WriteLine($"The folder {sourceDirectoryName} is archived to a file {destinationArchiveFileName}");
-        break;
-    case "2":
-        Write("Name of the source archive file: ");
-        string sourceArchiveFileName = ReadLine();
-        Write("Name of the target directory: ");
-        string destinationDirectoryName = ReadLine();
-        ZipFile.ExtractToDirectory(sourceArchiveFileName, destinationDirectoryName);
-        WriteLine();
-        WriteLine($"The file {sourceArchiveFileName} is unpacked to a folder {destinationDirectoryName}");
-        break;
-    default:
-        break;
+    WriteLine("Select action:\n" + "1 - create archive\n" + "2 - unpack archive\n" + "3 - exit\n");
+
+    int choice = Convert.ToInt32(ReadLine());
+
+    switch (choice)
+    {
+        case 1:
+            try
+            {
+                Write("Name of the source directory: ");
+                string sourceDirectoryName = ReadLine();
+                Write("File name of the target archive: ");
+                string destinationArchiveFileName = ReadLine();
+                ZipFile.CreateFromDirectory(sourceDirectoryName, destinationArchiveFileName, CompressionLevel.SmallestSize, true);
+                WriteLine();
+                WriteLine($"The folder {sourceDirectoryName} is archived to a file {destinationArchiveFileName}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            break;
+        case 2:
+            try
+            {
+                Write("Name of the source archive file: ");
+                string sourceArchiveFileName = ReadLine();
+                Write("Name of the target directory: ");
+                string destinationDirectoryName = ReadLine();
+                ZipFile.ExtractToDirectory(sourceArchiveFileName, destinationDirectoryName);
+                WriteLine();
+                WriteLine($"The file {sourceArchiveFileName} is unpacked to a folder {destinationDirectoryName}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            break;
+        case 3:
+            isOpen = false;
+            break;
+        default:
+            Console.WriteLine("Invalid command entered\n");
+            break;
+    }
+    ReadKey();
+    Clear();
 }
-ReadKey();
